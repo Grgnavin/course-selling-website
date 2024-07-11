67cf11843067826187460b6a43e9f81eb4a8fb9a
Navin grg <grgnabin60@gmail.com>
2024-07-11 13:35:04 +0545

    Added ApiResponse & ApiError

---
+++ src/utils/ApiError.ts
@@ -0,0 +1,15 @@
+
+class ApiEror<T> extends Error{
+    public success: boolean;
+    public data: T;
+    public message: string = "Something went wrong";
+
+    constructor(data: T, message: string){
+        super(message)
+        this.data = data;
+        this.message = message;
+        this.success = true;
+    }
+}
+
+export { ApiEror }
